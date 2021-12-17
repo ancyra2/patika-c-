8d6439f007a980fb1806ec538b884f0d5ced26f0
ancyra2 <duran.talih@gmail.com>
2021-12-17 16:42:51 +0300

    Ödev sisteme yüklendi

---
+++ consoleapp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace consoleapp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ödev");
+            Console.ReadLine();
+        }
+    }
+}
